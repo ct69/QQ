6a4061af1f2e11db9e83b68bcc7cde20c94fc8b6
CT <ct@lxtekimacbook-air.local>
2016-01-11 09:38:26 +0800

    Initial Commit

---
+++ QQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQ
+//
+//  Created by CT on 16/1/11.
+//  Copyright © 2016年 CT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QQ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QQ
+//
+//  Created by CT on 16/1/11.
+//  Copyright © 2016年 CT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
